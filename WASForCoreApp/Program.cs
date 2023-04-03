84325934dddcfa446ab58cb28b8726fe7b14de3e
wherewhere <qq2518766683@outlook.com>
2023-04-03 21:14:20 +0800

    借 WAS 还魂

---
+++ WASForCoreApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Windows.ApplicationModel.Core;
+using WinRT;
+
+namespace WASForCoreApp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            ComWrappersSupport.InitializeComWrappers();
+            CoreApplication.Run(new App());
+        }
+    }
+}
